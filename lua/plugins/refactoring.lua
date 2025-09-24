b9a52d044d27ee2ef93672659e6b6f13af7541ee
Adi Setyono <adi.setyono@gmail.com>
2025-09-24 23:41:33 +0700

    add refactoring

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -21,20 +21,31 @@ return {
   -- Refactoring tool
   {
     "ThePrimeagen/refactoring.nvim",
-    keys = {
-      {
-        "<leader>r",
-        function()
-          require("refactoring").select_refactor({
-            show_success_message = true,
-          })
-        end,
-        mode = "v",
-        noremap = true,
-        silent = true,
-        expr = false,
-      },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
+    config = function()
+      require("refactoring").setup({
+        -- your custom config here (optional)
+      })
+    end,
+    -- keys = {
+    --   {
+    --     "<leader>r",
+    --     function()
+    --       require("refactoring").select_refactor({
+    --         show_success_message = true,
+    --       })
+    --     end,
+    --     mode = "v",
+    --     noremap = true,
+    --     silent = true,
+    --     expr = false,
+    --   },
+    -- },
+    lazy = false,
+    -- event = "VeryLazy",
     opts = {},
   },
 }
