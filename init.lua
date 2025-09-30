5e37c07aa2413761ef4cae10b8dd3568b6a9dad0
Adi Setyono <adi.setyono@gmail.com>
2025-09-30 19:26:34 +0700

    paste keep

--- init.lua
+++ init.lua
@@ -9,5 +9,7 @@ require("config.lazy")
 --
 -- vim.cmd("colorscheme base16-tokyodark")
 
+-- paste without overwriting
+vim.keymap.set("v", "p", "P")
 vim.cmd("colorscheme citruszest")
 vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#87CEEB" }) -- Change header color
